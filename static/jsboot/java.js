cf0719483837cfd1cdf750277f45601e3568c4da
greenmeg <grmeg@umich.edu>
2016-04-23 15:30:18 -0400

    making more pages!

--- static/jsboot/java.js
+++ static/jsboot/java.js
@@ -51,3 +51,11 @@ $(document).ready(function(){
         });
    });
 })
+
+$('.thumbnail').click(function(){
+    $('.modal-body').empty();
+    var title = $(this).parent('a').attr("title");
+    $('.modal-title').html(title);
+    $($(this).parents('div').html()).appendTo('.modal-body');
+    $('#myModal').modal({show:true});
+});
